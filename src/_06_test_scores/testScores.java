75171b84e0a470eba486bcf27913629a61830b7b
League Student <league@iMac-15.attlocal.net>
2019-09-16 18:25:51 -0700

    :art: (IP)
    hi

---
+++ src/_06_test_scores/testScores.java
@@ -0,0 +1,12 @@
+package _06_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class testScores {
+public static void main(String[] args) {
+String test = JOptionPane.showInputDialog("what is your test score?");
+int tests = Integer.parseInt(test);
+double score  = 0.0;
+score = score + tests
+}
+}
